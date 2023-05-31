285fbf0231e42ceb1af9bb050acdbbf65ad78989
Henrietta <hetty8004@gmail.com>
2023-05-31 15:24:55 +0200

    learning to automate Tests learning to automate tests

---
+++ cypress/e2e/myfirsttest.cy.js
@@ -0,0 +1,14 @@
+describe('My first test', () => 
+{
+  it('verify: title-positive', () => 
+  {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM')
+  })
+
+  it('verify: title-Negative', () => 
+  {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM123')
+  })
+})
